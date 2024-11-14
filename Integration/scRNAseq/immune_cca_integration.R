fa4dc13a0e1fac585cd46a7d9e6aecb9d4bfaf70
Anne Victoria Forsyth <tnz0210@quser32.quest.it.northwestern.edu>
2024-11-14 09:00:29 -0600

    Added immune integration

---
+++ Integration/scRNAseq/immune_cca_integration.R
@@ -0,0 +1,57 @@
+# ------------------------------------------------------------------------------
+# -----                                                                    -----
+# -----                        AN1792 Project                              -----
+# -----                                                                    -----
+# -----                           Gate Lab                                 -----
+# -----                     Northwestern University                        -----
+# -----                                                                    -----
+# ------------------------------------------------------------------------------
+#
+# Written by: Anne Forsyth
+# Summary: Run SCTransform and perform CCA integration of immune cells in Seurat
+#
+#-------------------------------------------------------------------------------
+# Initialization
+
+# Load libraries
+suppressMessages({
+  library("plyr")
+  library("dplyr")
+  library("tidyverse")
+  library("Seurat")
+})
+
+# Load integrated scRNAseq Seurat object
+s <- readRDS("/path/to/integrated/scRNAseq/object.rds")
+
+# Subset for initially defined immune subtypes
+s <- subset(s, broad_anno_long %in% c("microglia/macrophage", "T lymphocyte", "monocyte"))
+DefaultAssay(s) <- "RNA"
+s <- DietSeurat(s, assays = "RNA")
+gc()
+
+# Create variable for integration groups (AN1792, CAA, LCMB)
+s$pool_merged <- as.character(s$pool)
+s$pool_merged[grep("^AN1792", s$pool)] <- "AN1792"
+s$pool_merged[grep("^LCMB", s$pool)] <- "LCMB"
+unique(s$pool_merged)
+
+# Split layers by merged sample ID 
+s <- JoinLayers(s)
+s[["RNA"]] <- split(s[["RNA"]], f = s$pool_merged)
+
+# Re-run SCTransform, PCA and IntegrateLayers on immune subset
+s <- SCTransform(s, assay = "RNA", vars.to.regress = "percent.mt")
+s <- RunPCA(s, verbose = F)
+s <- IntegrateLayers(object = s, method = CCAIntegration, normalization.method = 'SCT', orig.reduction = 'pca', verbose = F, new.reduction = "cca")
+
+# Remove old clustering from meta data
+s@meta.data[,grep("harmony", colnames(s@meta.data))] <- NULL
+s$seurat_clusters <- NULL
+
+# Save object 
+saveRDS(s, "/path/to/integrated/scRNAseq/immune/object.rds")
+
+
+
+
